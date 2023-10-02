61f2c524033e3107f0b0d95b69d8903bbd790490
Chrisluna10 <chrisluna4224@gmail.com>
2023-10-01 20:18:09 -0700

    adds pages as well as status colors

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,76 @@
+import TicketCard from '../components/TicketCard'
+import img from '../images/chris.jpg'
+
+const Dashboard = () => {
+
+    const tickets = [
+        {
+            category: 'Q1 2023',
+            color: 'red',
+            title: 'NFT Safety 101 Video',
+            owner: 'Chris Luna',
+            avatar: `${img}`,
+            status: 'done',
+            priority: 5,
+            progress: 40,
+            description: 'Make a video showcasing how to work with NFTs safely, including how to know if on eis not genuine.',
+            timestamp: '2023-09-11T07:36:17+0000'
+        },
+        {
+            category: 'Q1 2023',
+            color: 'red',
+            title: 'Build and Sell AI model',
+            owner: 'Chris Luna',
+            avatar: `${img}`,
+            status: 'working on it',
+            priority: 4,
+            progress: 70,
+            description: 'Make a video about AI.',
+            timestamp: '2023-09-13T07:36:17+0000'
+        },
+        {
+            category: 'Q2 2023',
+            color: 'blue',
+            title: 'Build a bot',
+            owner: 'Chris Luna',
+            avatar: `${img}`,
+            status: 'working on it',
+            priority: 3,
+            progress: 10,
+            description: 'Make a video about making a bot.',
+            timestamp: '2023-09-15T07:36:17+0000'
+        }
+    ]
+
+    const uniqueCategories = [
+        ...new Set(tickets?.map(({ category }) => category))
+    ]
+
+    console.log(uniqueCategories)
+
+    return (
+        <div className="dashboard">
+            <h1>My Projects</h1>
+            <div className="ticket-container">
+                {tickets && uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+                    <div key={categoryIndex}>
+                        <h3>{uniqueCategory}</h3>
+                        {tickets.filter(ticket => ticket.category === uniqueCategory)
+                            .map((filteredTicket, _index) => (
+                                <TicketCard
+                                    id={_index}
+                                    color={filteredTicket.color}
+                                    ticket={filteredTicket}
+                                />
+                            ))
+                        }
+                    </div>
+                ))}
+
+            </div>
+        
+        </div>
+    )
+}
+
+export default Dashboard
